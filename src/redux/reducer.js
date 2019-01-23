d9dca308427e50adb38aec67d035aaa93da28db3
Young <youngb0@c02xm4lrjgh6.us.maritz.net>
2019-01-23 15:42:43 -0600

    removed unnecessary code

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,30 +1,28 @@
 import {
-    createSelector
+  createSelector
 } from 'reselect'
 
 import {
-    ADD_ONE,
-    MINUS_ONE
+  ADD_ONE,
+  MINUS_ONE
 } from './actions.js'
 
 const INITIAL_STATE = {
-    count: 0
+  count: 0
 }
 
 export const reducer = (state = INITIAL_STATE, action) => {
-    // debugger
-    switch (action.type) {
-        case ADD_ONE:
-            return {
-                ...state,
-                count: state.count + 1
-            }
-        case MINUS_ONE:
-            return {
-                ...state,
-                count: state.count - 1
-            }
-        default:
-            return state
-    }
+  // debugger
+  switch (action.type) {
+    case ADD_ONE:
+      return {
+        count: state.count + 1
+      }
+    case MINUS_ONE:
+      return {
+        count: state.count - 1
+      }
+    default:
+      return state
+  }
 }
